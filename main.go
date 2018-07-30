c53d07321b5ba4ddbbcccd79208cd2703e3a1430
Ritesh Chauhan <chauhanx.ritesh@gmail.com>
2018-07-30 18:13:37 +0530

    adding cgroup support

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"os/exec"
 	"syscall"
+	"path/filepath"
+	"io/ioutil"
+	"strconv"
 )
 
 func main(){
@@ -24,18 +27,31 @@ func run(){
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
 	}
 	must(cmd.Run())
 }
 
 func child(){
 	fmt.Printf("running %v as pid: %d\n", os.Args[2:], os.Getpid())
+	cg()
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	must(cmd.Run())
+}
+
+func cg(){
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups,"pids")
+	fmt.Println("Creating.. pids at: ",filepath.Join(pids,"chauhr"))
+
+	must(os.MkdirAll(filepath.Join(pids,"chauhr"), 0755))
+	must(ioutil.WriteFile(filepath.Join(pids, "chauhr/pids.max"), []byte("10"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids,"chauhr/notify_on_release"), []byte("1"), 0700)) // release the cgroup when the container ends.
+	must(ioutil.WriteFile(filepath.Join(pids,"chauhr/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())),0700))
 }
 
 func must (err error){
