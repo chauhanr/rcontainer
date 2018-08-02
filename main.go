5d059a641cec7ea3d7ffbb1eaedad62d66a59a22
Ritesh Chauhan <chauhanx.ritesh@gmail.com>
2018-08-02 11:13:38 +0530

    adding reexec support

--- main.go
+++ main.go
@@ -4,47 +4,76 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
+	"fmt"
+	"github.com/docker/docker/pkg/reexec"
 )
 
 func main(){
-	switch os.Args[1]{
-		case "run":
-			run()
-	  	default:
-			panic("command not supported")
-	}
-}
-
-func run(){
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
-	cmd.Stdout = os.Stdout
+	cmd := reexec.Command("nsInitialization")
 	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	cmd.Env = []string{"PS1=rcontainer #"}
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS |
-			syscall.CLONE_NEWNS |
+		Cloneflags: syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWUTS |
 			syscall.CLONE_NEWIPC |
 			syscall.CLONE_NEWPID |
 			syscall.CLONE_NEWNET |
-			syscall.CLONE_NEWUSER ,
+			syscall.CLONE_NEWUSER,
 		UidMappings: []syscall.SysProcIDMap{
-				{
-					ContainerID: 0,
-					HostID: os.Getuid(),
-					Size: 1,
-				},
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
 		},
 		GidMappings: []syscall.SysProcIDMap{
 			{
 				ContainerID: 0,
-				HostID:    os.Getgid(),
-				Size:      1,
+				HostID:      os.Getgid(),
+				Size:        1,
 			},
 		},
 	}
-	must(cmd.Run())
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Error running the reexec.Command - %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func init (){
+	reexec.Register("nsInitialization", nsInitialization)
+	if reexec.Init(){
+		os.Exit(0)
+	}
+}
+
+func nsInitialization(){
+	fmt.Printf("\n>> namespace setup code goes here << \n\n")
+	/*switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("command not supported")
+	}*/
+	run()
+}
+
+func run(){
+	//cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/bin/sh")
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+
+	cmd.Env = []string{"PS1=-[ns-process]- # "}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Error running the /bin/sh command - %s\n", err)
+		os.Exit(1)
+	}
 }
 
 func must (err error){
